7001403b2fa4a61845444a697170d6c040d94898
姚正发 <976691141@qq.com>
2017-07-05 11:12:42 +0800

    增加测试控制台

---
+++ test/ConsoleDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using Sino.WilddogClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoleDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var wilddog = new WilddogClient("");
+            var child = wilddog.Child("eventdata");
+            var node = child.Child("13.json");
+
+            string s = node.Insert(new
+            {
+                A = 1,
+                B = "123"
+            });
+        }
+    }
+}
